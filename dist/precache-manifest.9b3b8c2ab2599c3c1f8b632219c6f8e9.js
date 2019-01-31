888a3dd415d6d055b7e2ee6984fabe576cabe303
savagemechanic <seighaf@yahoo.com>
2019-01-31 12:43:40 +0100

    Adding files.

---
+++ dist/precache-manifest.9b3b8c2ab2599c3c1f8b632219c6f8e9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "339845efb3bdd792154f",
+    "url": "/js/cart.4c694215.js"
+  },
+  {
+    "revision": "b0f291ca45af1ad90005",
+    "url": "/js/chunk-vendors.a5983931.js"
+  },
+  {
+    "revision": "c849be789ee5c511f267",
+    "url": "/js/app.8e7cf8e3.js"
+  },
+  {
+    "revision": "2ee3b97a4940cf340fdd55976edd95eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "b0f291ca45af1ad90005",
+    "url": "/css/chunk-vendors.8e2ccd0c.css"
+  },
+  {
+    "revision": "c849be789ee5c511f267",
+    "url": "/css/app.162501db.css"
+  }
+];
