1e5155b153a79582dc16a1474663d72bc7f00b69
savagemechanic <seighaf@yahoo.com>
2019-02-04 05:16:22 +0100

    re enable git ignore

---
+++ dist/precache-manifest.b32c2aca655a0e15f2fe04a4048ccbbc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3faa9a2d0a4dccd155eb87d43283131",
+    "url": "/img/bag.svg"
+  },
+  {
+    "revision": "868e9ac0cf004f51a35058859e98d6b1",
+    "url": "/img/shoes@2x.png"
+  },
+  {
+    "revision": "63f70ac0198b31be8eddcec702319b0d",
+    "url": "/img/shoes@3x.png"
+  },
+  {
+    "revision": "53126ed8bb78c653a851",
+    "url": "/js/cart.7125d229.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "021ce91e67bb357b156b",
+    "url": "/js/chunk-vendors.51738327.js"
+  },
+  {
+    "revision": "ae0a1c56042664645b84b6c74dcadee5",
+    "url": "/listings.json"
+  },
+  {
+    "revision": "cf762c865b15c13f73a2",
+    "url": "/js/app.46f4cc9c.js"
+  },
+  {
+    "revision": "3fa9c6174750343ae18e62d1ad4e614e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8aba9f018ab84ec256fd53ec4a42e81",
+    "url": "/img/mens-smart-winter-dressing_1024x1024.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "9f1585bd20dda8ac44f6108ce76817d6",
+    "url": "/img/cart.svg"
+  },
+  {
+    "revision": "63f70ac0198b31be8eddcec702319b0d",
+    "url": "/img/shoes-copy-2@3x.png"
+  },
+  {
+    "revision": "f5a7389cfc61bcd50aa269782e627468",
+    "url": "/img/451621_BXOA0_1060_001_100_0000_Light-Leather-high-top-with-tiger.jpg"
+  },
+  {
+    "revision": "112d7dac6a6ff6ae40f9c5166d4b1dc6",
+    "url": "/img/close-circled.svg"
+  },
+  {
+    "revision": "9fb6ab0f1f24b45e37b89e46c83eabf3",
+    "url": "/img/Feature-11.jpg"
+  },
+  {
+    "revision": "e4f31440aae0dd7c3f9224e113be1a43",
+    "url": "/img/shoes.png"
+  },
+  {
+    "revision": "ddcc65bcf9cdf5d4e2dc26afef9e7c32",
+    "url": "/img/sd-2018-mens-clothing-cat-1-v2-mob.jpg"
+  },
+  {
+    "revision": "08260962ee0444fca78869671f56794e",
+    "url": "/img/npdQPRoEF7fAsnA9kvESQG-768-80.jpg"
+  },
+  {
+    "revision": "868e9ac0cf004f51a35058859e98d6b1",
+    "url": "/img/shoes-copy-2@2x.png"
+  },
+  {
+    "revision": "e4f31440aae0dd7c3f9224e113be1a43",
+    "url": "/img/shoes-copy-2.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "021ce91e67bb357b156b",
+    "url": "/css/chunk-vendors.8e2ccd0c.css"
+  },
+  {
+    "revision": "53126ed8bb78c653a851",
+    "url": "/css/cart.64a6687f.css"
+  },
+  {
+    "revision": "cf762c865b15c13f73a2",
+    "url": "/css/app.292f956e.css"
+  }
+];
